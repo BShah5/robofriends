de8aefda66aa9d6f8dff6fc5c0ef872f5faf33fe
BShah <thisisbhoomishah@gmail.com>
2020-05-04 13:48:25 +0530

    Updates

---
+++ precache-manifest.383648b5d8073da2b4d387ec4bc6ed46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22173fd44076f784d7751e587213a337",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "155520c129548449f706",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "b289a4e635a67e642d27",
+    "url": "/robofriends/static/css/main.eefeef9f.chunk.css"
+  },
+  {
+    "revision": "155520c129548449f706",
+    "url": "/robofriends/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b289a4e635a67e642d27",
+    "url": "/robofriends/static/js/main.c44bc785.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
